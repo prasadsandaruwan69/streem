5c2e2c56f5a5587898fc645229459fb7db01e183
prasad sandaruwan <prasadsandaruwan069@gmail.com>
2026-02-23 18:18:08 +0530

    Create streem.py3322
    23

---
+++ streem.py
@@ -0,0 +1,30 @@
+import cv2
+from flask import Flask, Response
+import time
+
+app = Flask(_name_)
+
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('U','Y','V','Y'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 60)
+
+def generate_frames():
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + 
+               frame_bytes + b'\r\n')
+
+@app.route('/cam/1')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=9000, debug=False)
